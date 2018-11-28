48991f7ac373efbb84d41d3617058fe16e9a87fc
chenhao1024 <brunetsheet@qq.com>
2018-11-28 20:10:18 +0800

    commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app,db
-from flask import render_template,flash,redirect,url_for,request
+from flask import render_template,flash,redirect,url_for,request,g
 from flask_login import current_user, login_user, logout_user,login_required
 from app.models import User,Post
 from app.forms import LoginForm,RegistrationForm,EditProfileForm,PostForm
@@ -7,6 +7,8 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from app.forms import ResetPasswordRequestForm,ResetPasswordForm
 from app.email import send_password_reset_email
+from guess_language import guess_language
+from flask_babel import _, get_locale
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
@@ -14,10 +16,13 @@ from app.email import send_password_reset_email
 def index():
   form = PostForm()
   if form.validate_on_submit():
-    post = Post(body=form.post.data,author=current_user)
+    language = guess_language(form.post.data)
+    if language == "UNKNOWN" or len(language) > 5:
+      language = ''
+    post = Post(body=form.post.data,author=current_user,language=language)
     db.session.add(post)
     db.session.commit()
-    flash('Your post is now live!')
+    flash(_('Your post is now live!'))
     return redirect(url_for('index'))
   page = request.args.get('page',1,type=int)
   # posts = current_user.followed_posts().all()
@@ -37,7 +42,7 @@ def login():
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username.data).first()
-    print(user.username)
+    # print(user.username)
     if user is None or not user.check_password(form.password.data):
       flash('Invalid username or password')
       return redirect(url_for('login'))
@@ -94,6 +99,7 @@ def before_request():
   if current_user.is_authenticated:
     current_user.last_seen = datetime.utcnow()
     db.session.commit()
+  g.locale = str(get_locale())
 
 
 @app.route('/edit_profile',methods=['GET','POST'])
